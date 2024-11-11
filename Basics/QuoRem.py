8539e5f2aaa2abac8380535cf178be7929ad9332
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-11-11 10:02:50 +0530

    [Sudhanshu] Update: Updated QuoRem.py

--- Basics/QuoRem.py
+++ Basics/QuoRem.py
@@ -1,4 +1,28 @@
+'''
+    @Author: Sudhanshu Kumar
+    @Date: 20-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 11-11-2024
+    @Title : Quotient and Remainder
 
+'''
+
+import random
+
+num = int(input("Enter no. of times to flip the coin: "))
+count_head = 0
+count_tail = 0
+for x in range(0,num):
+    random_num = random.random();
+    if random_num<0.5:
+        count_tail+=1
+    else:
+        count_head+=1
+
+head_per = count_head*100/num
+tail_per = count_tail*100/num
+
+print(f"Head percentage: {head_per}  Tail percentage: {tail_per}")
 
 num = int(input("Enter a number: "))
 div = int(input("Enter divisor: "))
