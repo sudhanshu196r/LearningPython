895791ecaf58f8801bd7a5c13c270f14a542638e
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-11-11 09:58:33 +0530

    [Sudhanshu] Update: updated Largest.py

--- Basics/Largest.py
+++ Basics/Largest.py
@@ -1,4 +1,28 @@
+'''
+    @Author: Sudhanshu Kumar
+    @Date: 19-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 04-11-2024
+    @Title : Largest number
 
+'''
+
+import random
+
+num = int(input("Enter no. of times to flip the coin: "))
+count_head = 0
+count_tail = 0
+for x in range(0,num):
+    random_num = random.random();
+    if random_num<0.5:
+        count_tail+=1
+    else:
+        count_head+=1
+
+head_per = count_head*100/num
+tail_per = count_tail*100/num
+
+print(f"Head percentage: {head_per}  Tail percentage: {tail_per}")
 num1 = int(input("Enter first num: "))
 num2 = int(input("Enter second num: "))
 num3 = int(input("Enter third num: "))
